5fbdc1e5374c7d40392b383abb2cbd446d4bdeff
Daniel Courivaud <daniel.courivaud@esiee.fr>
2024-05-24 16:47:27 +0200

    initial commit

---
+++ primes.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+def isprime(p):
+
+# votre code ici
+        
+    return True
+
+def main():
+
+    for i in range(1, 100):
+        if isprime(i):
+            print(i)
+
+if __name__ == "__main__":
+    main()
